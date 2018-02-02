a9db1239a471fd6d1cb1c8d028d00913e60f18d6
Jake Shober <jake@Jakes-MacBook-Air.local>
2018-02-01 21:33:52 -0700

    3 tests written, hrefs incomplete.

--- nightwatch/test_data/css_selectors.js
+++ nightwatch/test_data/css_selectors.js
@@ -1,9 +1,10 @@
 module.exports = {
+    homePage: {
     topNavBtns: {
-        clipHome: 'div[class="logo"]',
-        contact: 'div[class="contact"]',
+        clipHome: 'a[href="https://clip.mx/"]',
+        contact: 'div[class="contact-information"]',
         account: 'a[href="https://dashboard.clip.mx/us/users/sign_in"]',
-        help: 'a[href=https://clip.mx/us/ayuda"]',
+        help: 'a[href="https://clip.mx/us/ayuda"]',
         buyReader: 'a[id="button_header_shop"]',
     },
     bannerBtns: {
@@ -12,19 +13,54 @@ module.exports = {
     },
     benefitsBtns: {
         meetClip: 'a[id="button_benefits_shop"]',
-        mobility: 'a[href="https://clip.mx/us/dispositivos"]',
-        sellMore: 'a[href="https://clip.mx/us/mensualidades"]',
-        easyToUse: 'a[class="wistia-lighbox"]',
-        security: 'a[href=https://clip.mx/us/seguridad"]',
+        mobility: 'a[href="https://clip.mx/us/dispositivos/"]',
+        sellMore: 'a[href="https://clip.mx/us/mensualidades/"]',
+        easyToUse: 'a[href="#"]',
+        security: 'a[href="https://clip.mx/us/seguridad/"]',
     },
     commissionsBtns: {
-        rate: 'select[name"=select"]',
-        calculate: 'input["class=btn btn-primary"]',
+        rate: 'select[name="select"]',
+        calculate: 'input[class="btn btn-primary"]',
         moreInfo1: 'a[href="https://clip.mx/us/comisiones-para-mensualidades"]',
         moreInfo2: 'a[href="https://clip.mx/us/comisiones-por-transaccion"]'
     },
     findStore: 'a[class="btn btn-primary"]',
     everyBusinessBtns: {
-        
+        retail: 'a[href="https://clip.mx/us/comercios"]',
+        professionalServices: 'a[href="https://clip.mx/us/servicios-profesionales"]',
+        foodDrink: 'a[href="https://clip.mx/us/alimentos"]',
+        beautyHealth: 'a[href="https://clip.mx/us/salud-y-belleza"]',
+        sportsEntertainment: 'a[href="https://clip.mx/us/deporte-y-entretenimiento"]',
+        estateConstruction: 'a[href="https://clip.mx/us/arquitectura-y-construccion"]',
+        accomodation: 'a[href="https://clip.mx/us/alojamiento"]',
+        transportation: 'a[href="https://clip.mx/us/alojamiento"]',
+        education: 'a[href="https://clip.mx/us/educacion"]',
+        registerNow: 'a[id="button_industries"]'
+    },
+    fastEasyBtns: {
+        registerNow: 'a[id="button_steps"]',
+        buyReader: 'a[id="button_steps_shop"]',
+        ios: 'a[href="https://itunes.apple.com/app/id771245360"]',
+        android: 'a[href="https://play.google.com/store/apps/details?id=com.payclip.clip"]'
+    },
+    helpBtns: {
+        call: 'a[href="https://clip.mx/us/tel:015563932323"]',
+        chat: 'a[href="https://clip.mx/us/contacto"]',
+        questions: 'a[href="https://clip.mx/us/ayuda"]'
+    },
+    bottomNavBtns: {
+        about: 'a[href="https://clip.mx/us/nosotros"]',
+        news: 'a[href="https://clip.mx/us/noticias"]',
+        blog: 'a[href="https://clip.mx/us/blog"]',
+        help: 'a[href="https://clip.mx/us/ayuda"]',
+        contact: 'a[href="https://clip.mx/us/contacto"]',
+        facebook: 'a[href="https://www.facebook.com/clipmx"]',
+        twitter: 'a[href="https://twitter.com/clip_mx"]',
+        youtube: 'a[href="https://www.youtube.com/user/clipmx/"]',
+        appStore: 'a[href="https://itunes.apple.com/app/id771245360"]',
+        googlePlay: 'a[href="https://play.google.com/store/apps/details?id=com.payclip.clip"]',
+        privacyPolicy: 'a[href="https://clip.mx/us/privacidad"]',
+        termsConditions: 'a[href="https://clip.mx/us/terminos"]'
     }
+}
 }
--- nightwatch/tests/jake_tests.js
+++ nightwatch/tests/jake_tests.js
@@ -1,13 +1,60 @@
 const selectors = require('../test_data/css_selectors')
 const data = require('../test_data/test_data')
-const func = require('../test_data/helper_functions')
 
 module.exports = {
     beforeEach: browser => {
-        browser.url('http://localhost:3000')
+        browser.url('https://clip.mx/us/'),
+        browser
+        .waitForElementVisible('body', 1000)
     },
     after: browser => {
         browser.end()
     },
-    
-        }
+    'Top NavBar Button Tests': browser => {
+        browser
+            .click(selectors.homePage.topNavBtns.contact)
+            .waitForElementVisible('body', 1000)        
+            browser.back
+        browser
+            .click(selectors.homePage.topNavBtns.help)
+            .waitForElementVisible('body', 1000)
+        browser.back
+        browser
+            .click(selectors.homePage.topNavBtns.account)
+            .waitForElementVisible('body', 1000)
+        browser.back
+        browser
+            .click(selectors.homePage.topNavBtns.buyReader)
+            .waitForElementVisible('body', 1000)
+            .click(selectors.homePage.topNavBtns.clipHome)
+    },
+    'Banner Button Tests': browser => {
+        browser
+        .click(selectors.homePage.bannerBtns.buyReader)
+        .waitForElementVisible('body', 1000)
+        browser.back
+        browser
+        .click(selectors.homePage.bannerBtns.registerNow)
+    },
+    'Benefits Section Button Tests': browser => {
+        browser
+        .click(selectors.homePage.benefitsBtns.meetClip)
+        .waitForElementVisible('body', 1000)
+        browser.back
+        browser
+        .click(selectors.homePage.benefitsBtns.mobility)
+        .waitForElementVisible('body', 1000)
+        browser.back
+        browser
+        .click(selectors.homePage.benefitsBtns.sellMore)
+        .waitForElementVisible('body', 1000)
+        browser.back
+        browser
+        .click(selectors.homePage.benefitsBtns.easyToUse)
+        .waitForElementVisible('body', 1000)
+        browser.back
+        browser
+        .click(selectors.homePage.benefitsBtns.security)
+    },
+
+}
